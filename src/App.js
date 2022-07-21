d2254e0c3ee0301ae8428eee146261b5ea06aea8
firsticz <first@thai.run>
2022-07-22 01:26:07 +0700

    update card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import { Container,  } from '@nextui-org/react'
+import { Container, Grid } from '@nextui-org/react'
 import Header from './components/common/header'
+import Card from './components/ScriptCard/index'
 
 function App() {
   return (
       <Container>
         <Header />
         <Container>
-          test
+          <Grid.Container gap={2} justify="center">
+            <Grid xs={4}>
+              <Card />
+            </Grid>
+            <Grid xs={4}>
+              <Card />
+            </Grid>
+            <Grid xs={4}>
+              <Card />
+            </Grid>
+          </Grid.Container>
+          
         </Container>
       </Container>
   );
